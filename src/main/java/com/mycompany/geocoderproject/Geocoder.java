0479dc0b92a5a86410bba35d2a142793e8f0f167
kowlad <wladickowalew@gmail.com>
2020-05-26 14:14:09 +0300

    first

---
+++ src/main/java/com/mycompany/geocoderproject/Geocoder.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.geocoderproject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author ROOT
+ */
+public class Geocoder {
+    
+    public static String[] getObjects(String query){
+        try {
+            String URLstr = getURL(query);
+            URL url = new URL(URLstr);
+            System.out.println("URL: " + url);
+            JSONArray jsonArray = getJSONArray(url);
+            for (int i = 0; i < jsonArray.length(); i++){
+                System.out.println(jsonArray.get(i));
+            }
+        } catch (UnsupportedEncodingException ex) {
+            System.out.println("Ошибка декодирования");
+            Logger.getLogger(Geocoder.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            System.out.println("Ошибка Формирования URL");
+            Logger.getLogger(Geocoder.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            System.out.println("Ошибка запроса");
+            Logger.getLogger(Geocoder.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JSONException ex) {
+            System.out.println("Ошибка JSON");
+            Logger.getLogger(Geocoder.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return new String[]{"1", "2", "8"};
+    }
+    
+    private static String getURL(String query) throws UnsupportedEncodingException{
+        String server = "https://geocode-maps.yandex.ru/1.x/";
+        String params = "format=json" +
+                        "&results=100" +
+                        "&geocode=" + URLEncoder.encode(query, "UTF-8") +
+                        "&apikey=" + Secret.getKEY();
+        return server + "?" + params;
+    }
+    
+    private static JSONArray getJSONArray(URL url) throws IOException, JSONException{
+        HttpURLConnection con = (HttpURLConnection)url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader in = new BufferedReader(
+                                new InputStreamReader(
+                                    con.getInputStream()));
+        String tmp;
+        StringBuilder builder = new StringBuilder();
+        while((tmp = in.readLine()) != null)
+            builder.append(tmp);
+        in.close();
+        
+        JSONObject JSON = new JSONObject(builder.toString());
+        JSONArray ans = JSON.getJSONObject("response")
+                            .getJSONObject("GeoObjectCollection")
+                            .getJSONArray("featureMember");
+        return ans;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/geocoderproject/Secret.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.geocoderproject;
+
+/**
+ *
+ * @author ROOT
+ */
+public class Secret {
+    
+    public static String getKEY(){
+        return "a880cf10-7c51-4526-9e32-517f6e45c777";
+    }
+    
+}
